76528b79d85185f6ca3fa4ecf7d528bc574c842e
tallua <talluay@gmail.com>
2022-01-07 10:20:49 +0000

    update response

--- aws-loabot-router/src/main.cpp
+++ aws-loabot-router/src/main.cpp
@@ -32,28 +32,56 @@ void LOG(_Args&&... args)
 
 }
 
+namespace discord {
+
+enum class RequestType {
+    Ping = 1,
+    Command = 2
+};
+
+}
+
 int main(int, char**)
 {
     Aws::InitAPI(Aws::SDKOptions());
 
     aws::lambda_runtime::run_handler([](const invocation_request& req) {
         LOG(req.payload);
-        const JsonValue payload(req.payload);
+        const auto payload = JsonValue(req.payload);
 
         if (!discord::validate_message(payload.View())) {
             LOG("Error: validation failed");
             return invocation_response::failure("Invalid Request", "400");
         }
-
         LOG("Success: validation success");
-        return invocation_response::success(R"({
-            "statusCode": 200,
-            "isBase64Encoded": false,
-            "headers": {
-                "content-type": "application/json"
-            },
-            "body": "{\"type\": 1 }"
-        })", "application/json");
+
+        const auto body = JsonValue(payload.View().GetString("body"));
+        const auto message_type = body.View().GetInteger("type");
+        LOG("Message Type: ", message_type);
+
+        switch(discord::RequestType(message_type))
+        {
+        case discord::RequestType::Ping:
+            return invocation_response::success(R"({
+                "statusCode": 200,
+                "isBase64Encoded": false,
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "body": "{\"type\": 1 }"
+            })", "application/json");
+        case discord::RequestType::Command:
+            return invocation_response::success(R"({
+                "statusCode": 200,
+                "isBase64Encoded": false,
+                "headers": {
+                    "content-type": "application/json"
+                },
+                "body": "{\"type\": 1 }"
+            })", "application/json");
+        default:
+            return invocation_response::failure("Unknown Message Type", "404");
+        }
     });
 
     return 0;
