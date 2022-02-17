bea6db2e3861f66eb4ed4418cbe9f6e113a74beb
tallua <talluay@gmail.com>
2022-02-17 01:58:25 +0000

    implement loabot handler

--- aws-loabot-handler/include/loabot/loabot_fetch.hpp
+++ aws-loabot-handler/include/loabot/loabot_fetch.hpp
@@ -14,17 +14,31 @@ public:
     virtual const CharacterData& FetchCharacter() = 0;
     virtual const StatData& FetchStat() = 0;
     virtual const MokokoData& FetchMokoko() = 0;
+    virtual const CollectionData& FetchIslandHeart() = 0;
+    virtual const CollectionData& FetchIgneaToken() = 0;
+    virtual const CollectionData& FetchOrpheusStar() = 0;
+    virtual const CollectionData& FetchGiantHeart() = 0;
+    virtual const CollectionData& FetchMasterpiece() = 0;
+    virtual const CollectionData& FetchSeaBounties() = 0;
+    virtual const CollectionData& FetchWorldTreeLeaf() = 0;
 };
 
 class LoaHomepageDataFetcher : public DataFetcher {
 public:
-    LoaHomepageDataFetcher();
+    LoaHomepageDataFetcher(const std::string& name);
     ~LoaHomepageDataFetcher();
 
 public:
     const CharacterData& FetchCharacter() override;
     const StatData& FetchStat() override;
     const MokokoData& FetchMokoko() override;
+    const CollectionData& FetchIslandHeart() override;
+    const CollectionData& FetchIgneaToken() override;
+    const CollectionData& FetchOrpheusStar() override;
+    const CollectionData& FetchGiantHeart() override;
+    const CollectionData& FetchMasterpiece() override;
+    const CollectionData& FetchSeaBounties() override;
+    const CollectionData& FetchWorldTreeLeaf() override;
 
 private:
     struct Context;
--- aws-loabot-handler/src/loabot_handler.cpp
+++ aws-loabot-handler/src/loabot_handler.cpp
@@ -2,6 +2,9 @@
 
 #include <aws/core/http/HttpResponse.h>
 
+#include <algorithm>
+#include <array>
+
 #include "loabot/loabot_fetch.hpp"
 #include "loabot/loabot_http.hpp"
 #include "loabot/loabot_response.hpp"
@@ -27,39 +30,106 @@ JsonView find_option(const Aws::Utils::Array<JsonView>& options,
     throw std::out_of_range("name not exist: " + name);
 }
 
+using namespace std::literals::string_literals;
+std::array mokoko_alias{"모코코의 씨앗"s, "모코코"s};
+std::array island_alias{"섬의 마음"s, "섬마"s};
+std::array tome_alias{"이그네아의 징표"s, "이그네아"s, "징표"s};
+std::array star_alias{"오르페우스의 별"s, "별"s};
+std::array giant_alias{"거인의 심장"s, "심장"s};
+std::array masterpiece_alias{"위대한 미술품"s, "미술품"s};
+std::array bounties_alias{"항해 모험물"s, "모험물"s};
+std::array leaf_alias{"세계수의 잎"s, "세계수"s};
 }  // namespace
 
 std::unique_ptr<discord::handler::DiscordHandler> LoabotBuilder::build(
-    Aws::Client::ClientConfiguration config) {
-    using discord::handler::Command;
-    using discord::message::Content;
-    using discord::message::Embed;
+    Aws::Client::ClientConfiguration) {
     using loabot::data::fetch::LoaHomepageDataFetcher;
 
     auto handler = std::make_unique<discord::handler::DiscordHandler>();
 
-    handler->add_command("캐릭터", [](const Command& command) {
-        const auto character_name =
-            find_option(command.payload.GetArray("options"), "이름")
-                .GetString("value");
+    handler->add_command(
+        "캐릭터", [](const discord::handler::Command& command) {
+            const auto character_name =
+                find_option(command.payload.GetArray("options"), "이름")
+                    .GetString("value");
 
-        auto fetcher = std::make_unique<LoaHomepageDataFetcher>();
+            auto character_fetcher =
+                std::make_unique<LoaHomepageDataFetcher>(character_name);
 
-        const auto& character = fetcher->FetchCharacter();
-        const auto& stat = fetcher->FetchStat();
+            const auto& character = character_fetcher->FetchCharacter();
+            const auto& stat = character_fetcher->FetchStat();
 
-        return response::format_character(character, stat);
-    });
-    handler->add_command("수집품", [](const Command& command) {
-        const auto character_name =
-            find_option(command.payload.GetArray("options"), "이름")
-                .GetString("value");
-        const auto collection_name =
-            find_option(command.payload.GetArray("options"), "종류")
-                .GetString("value");
+            return response::format_character(character, stat);
+        });
 
-        return Content{"조금만 기다려주세요..."};
-    });
+    handler->add_command(
+        "수집품", [](const discord::handler::Command& command) {
+            const auto character_name =
+                find_option(command.payload.GetArray("options"), "이름")
+                    .GetString("value");
+
+            auto character_fetcher =
+                std::make_unique<LoaHomepageDataFetcher>(character_name);
+
+            const auto collection_name =
+                find_option(command.payload.GetArray("options"), "종류")
+                    .GetString("value");
+            auto match_collection = [&](auto&& aliases) {
+                return std::find(aliases.begin(), aliases.end(),
+                                 collection_name) != aliases.end();
+            };
+
+            if (match_collection(mokoko_alias)) {
+                const auto& mokoko = character_fetcher->FetchMokoko();
+
+                return response::format_mokoko(mokoko);
+            }
+
+            if (match_collection(island_alias)) {
+                const auto& island = character_fetcher->FetchIslandHeart();
+
+                return response::format_collection(island);
+            }
+
+            if (match_collection(tome_alias)) {
+                const auto& tome = character_fetcher->FetchIgneaToken();
+
+                return response::format_collection(tome);
+            }
+
+            if (match_collection(star_alias)) {
+                const auto& star = character_fetcher->FetchOrpheusStar();
+
+                return response::format_collection(star);
+            }
+
+            if (match_collection(giant_alias)) {
+                const auto& giant = character_fetcher->FetchGiantHeart();
+
+                return response::format_collection(giant);
+            }
+
+            if (match_collection(masterpiece_alias)) {
+                const auto& masterpiece = character_fetcher->FetchMasterpiece();
+
+                return response::format_collection(masterpiece);
+            }
+
+            if (match_collection(bounties_alias)) {
+                const auto& bounties = character_fetcher->FetchSeaBounties();
+
+                return response::format_collection(bounties);
+            }
+
+            if (match_collection(leaf_alias)) {
+                const auto& leaves = character_fetcher->FetchWorldTreeLeaf();
+
+                return response::format_collection(leaves);
+            }
+
+            return discord::message::Content{collection_name +
+                                             "이 무엇인가요?"};
+        });
 
     return handler;
 }
